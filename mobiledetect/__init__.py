ffc9839247a08d3035f8e3b23b0f8a79dc435610
trbs <trbs@trbs.net>
2014-06-25 12:21:16 +0200

    version bumb

--- mobiledetect/__init__.py
+++ mobiledetect/__init__.py
@@ -6,7 +6,7 @@ Thanks to:
     https://github.com/serbanghita/Mobile-Detect/blob/master/Mobile_Detect.php
 """
 
-__version__ = "1.0.7"
+__version__ = "1.2.0"
 
 from .detect import MobileDetect  # NOQA
 
